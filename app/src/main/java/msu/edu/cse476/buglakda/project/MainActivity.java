afcb806cf6196f5f5aa5029a3391c0591da8b9cc
Danny Buglak <dannybuglak4@gmail.com>
2024-03-05 08:45:57 -0500

    First push

---
+++ app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
@@ -0,0 +1,14 @@
+package msu.edu.cse476.buglakda.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // Initial push
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
