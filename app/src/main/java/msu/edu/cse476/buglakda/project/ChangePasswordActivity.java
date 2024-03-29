a0ce3b0f1cffe8db9ebc2112ed1a6d7705d2d28f
Shashank Jayaram <jayara17@msu.edu>
2024-03-29 14:18:24 -0400

    buttons work

---
+++ app/src/main/java/msu/edu/cse476/buglakda/project/ChangePasswordActivity.java
@@ -0,0 +1,15 @@
+package msu.edu.cse476.buglakda.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+
+        // Add change password logic here
+    }
+}
--- app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
+++ app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
@@ -2,13 +2,31 @@ package msu.edu.cse476.buglakda.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    // Initial push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        Button buttonChangePassword = findViewById(R.id.buttonChange);
+
+        buttonLogin.setOnClickListener(v -> {
+            // Start LoginActivity
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start ChangePasswordActivity
+                startActivity(new Intent(MainActivity.this, ChangePasswordActivity.class));
+            }
+        });
     }
 }
