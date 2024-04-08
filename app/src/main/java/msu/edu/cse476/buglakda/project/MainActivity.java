e41e7dd70e96bbd27fc66c864ca9848a62e4293c
Shashank Jayaram <jayara17@msu.edu>
2024-04-08 03:00:26 -0400

    Changed to Login Activity, Hardcoded Password

--- app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
+++ app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
@@ -2,31 +2,13 @@ package msu.edu.cse476.buglakda.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-        Button buttonChangePassword = findViewById(R.id.buttonChange);
-
-        buttonLogin.setOnClickListener(v -> {
-            // Start LoginActivity
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        });
-
-        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start ChangePasswordActivity
-                startActivity(new Intent(MainActivity.this, ChangePasswordActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_main); // Ensure you have an `activity_main.xml` layout
+        // Add any main activity logic here
     }
 }
