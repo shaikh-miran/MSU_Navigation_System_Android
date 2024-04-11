0ac3d4bcb89a3e2684e0e83d3dd13de891461572
Danny Buglak <dannybuglak4@gmail.com>
2024-04-11 19:37:35 -0400

    Save marker and polylines on orientation change. Drop down menu for location

--- app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
+++ app/src/main/java/msu/edu/cse476/buglakda/project/MainActivity.java
@@ -22,7 +22,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,11 +50,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private final static double ENGLAT = 42.724303;
+    private Bundle thisSavedInstanceState;
 
+    /**
+     * Static constants for predefined locations of buildings for image
+     */
+    private final static double ENGLAT = 42.724303;
     private final static double ENGLONG = -84.480507;
 
     private final static double STEMLAT = 42.7265543;
@@ -59,8 +67,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private final static double BROADLONG = 42.7265538;
     private final static double BROADLAT = -84.4951027;
+
     /**
-     * Constants for setting of where user is going
+     * Constants for setting of where user is going for SharedPreferences
      */
     private final static String TO = "to";
     private final static String TOLAT = "tolat";
@@ -79,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private boolean valid = false;
 
     /**
-     * Location user is going to
+     * Destination location variables
      */
     private double toLatitude = 0;
     private double toLongitude = 0;
@@ -91,6 +100,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
 
     /**
+     * Store Destination address
+     */
+    private String dest;
+
+    /**
      * Listener for current location
      */
     private ActiveListener activeListener = new ActiveListener();
@@ -100,12 +114,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      */
     private SharedPreferences settings = null;
 
+    /**
+     * Markers and polyline for directions to destination
+     */
     private Marker lastMarker = null;
     private Polyline lastPolyline = null;
 
+    /**
+     * Initializes the activity, sets up location and map components.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         settings = getSharedPreferences("my.app.packagename_preferences", Context.MODE_PRIVATE);
+
 
         if (Build.VERSION.SDK_INT >= 23) {
             if (ActivityCompat.checkSelfPermission(this,
@@ -122,6 +143,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         super.onCreate(savedInstanceState);
+        this.thisSavedInstanceState = savedInstanceState;
         setContentView(R.layout.activity_main);
 
         // Obtain the SupportMapFragment and get notified with the map is ready to be used.
@@ -129,19 +151,75 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Force the screen to stay on and bright
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Setup the Spinner
+        Spinner buildingSpinner = findViewById(R.id.buildingSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.building_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        buildingSpinner.setAdapter(adapter);
+
+        // Spinner item selection listener
+        buildingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedBuilding = parent.getItemAtPosition(position).toString();
+                updateAddress(selectedBuilding);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //restorePolyline(savedInstanceState);
+
+        // Load persisted state from SharedPreferences
+        //loadPersistedMapState();
+
     }
 
+    /**
+     * Set the address for directions based on the choice selected
+     * @param buildingName Name of building chosen (Default: Broad)
+     */
+    private void updateAddress(String buildingName) {
+        switch (buildingName) {
+            case "Broad Business College":
+                dest = "632 Bogue St n520, East Lansing, MI";
+                break;
+            case "Engineering Building":
+                dest = "428 S Shaw Ln # 3115, East Lansing, MI 48824";
+                break;
+            case "STEM Building":
+                dest = "642 Red Cedar Rd, East Lansing, MI 48824";
+                break;
+            default:
+                dest = "Unknown";
+        }
+    }
+
+    /**
+     * Starts the SettingsActivity when the settings button is clicked
+     * @param view The view that was clicked
+     */
     public void onStartSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Helper method to initialize and set up the map fragment and location manager
+     */
     private void setupLocationManagerAndMapFragment() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Updates the user interface based on the current location by zooming in on current location
+     */
     private void setUI() {
         LatLng currLocation = new LatLng(latitude, longitude);
 
@@ -150,9 +228,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if (mMap != null && valid) {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 15));
+            loadPersistedMapState();
         }
     }
 
+    /**
+     * Handles the results of permission requests
+     * @param requestCode The request code passed in requestPermissions(android.app.Activity, String[], int)
+     * @param permissions The requested permissions
+     * @param grantResults The grant results for the corresponding permissions
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -172,28 +257,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    /**
+     * Starts the HelpActivity when the help button is clicked
+     * @param view The view that was clicked
+     */
     public void onStartHelp(View view) {
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Callback method to be invoked when the map is ready to be used
+     * @param googleMap A non-null instance of a GoogleMap associated with the SupportMapFragment
+     */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        //LatLng sydney = new LatLng(42.731138, -84.487508);
-        LatLng currLocation = new LatLng(latitude, longitude);
-
-        // TODO Temporary location, update coords to toLatitude and toLongitude once implemented
-        LatLng spartanStatue = new LatLng(42.731138, -84.487508);
-
         mMap = googleMap;
-
-        //mMap.addMarker(new MarkerOptions().position(spartanStatue).title("Spartan Statue"));
-
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 15));
-
-
-        // Request walking directions
-        //requestWalkingDirections(currLocation, spartanStatue);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            //loadPersistedMapState();
+        }
     }
 
     /**
@@ -216,6 +299,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         new FetchDirectionsTask().execute(requestUrl);
     }
 
+    /**
+     * AsyncTask to fetch walking directions from Google Maps Directions API
+     */
     private class FetchDirectionsTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
@@ -278,6 +364,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    /**
+     * Draws a polyline on the map using a series of LatLng points
+     * @param polylinePoints Encoded string representing the polyline points
+     */
     private void drawPolyline(String polylinePoints) {
         List<LatLng> decodedPath = PolyUtil.decode(polylinePoints);
         if (mMap != null) {
@@ -304,7 +394,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     /**
-     * Store current location
+     * Process a new location and change the image if necessary
      * @param location Current location
      */
     private void onLocation(Location location) {
@@ -331,6 +421,77 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         setUI();
     }
+
+    /**
+     * Saves the polyline's points into the Bundle for activity state restoration
+     * @param outState Bundle in which to place saved state data
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveInformation();
+    }
+
+    /**
+     * Restores the polyline from saved state data
+     * @param savedInstanceState Bundle containing the activity's previously saved state
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i("ONRESTORE", "############################");
+        super.onRestoreInstanceState(savedInstanceState);
+
+        loadPersistedMapState();
+    }
+
+    /**
+     * Restores the polyline if the map is ready and saved state contains polyline data
+     * @param savedInstanceState Bundle containing saved instance state
+     */
+    private void restorePolyline(Bundle savedInstanceState) {
+        if (mMap != null && savedInstanceState.containsKey("polyline_points")) {
+            ArrayList<LatLng> points = savedInstanceState.getParcelableArrayList("polyline_points");
+            if (points != null && !points.isEmpty()) {
+                lastPolyline = mMap.addPolyline(new PolylineOptions().addAll(points));
+            }
+        }
+    }
+
+    private void loadPersistedMapState() {
+
+        if (mMap == null) {
+            return;
+        }
+
+        SharedPreferences preferences = getSharedPreferences("my.app.packagename_preferences", MODE_PRIVATE);
+
+
+        // Load the polyline
+        String polylineJson = preferences.getString("polyline", null);
+        if (polylineJson != null) {
+            List<LatLng> points = convertJsonToPolylinePoints(polylineJson);
+            if (points != null && !points.isEmpty()) {
+                lastPolyline = mMap.addPolyline(new PolylineOptions().addAll(points));
+            }
+        }
+
+        // Load the marker
+        if (preferences.contains("marker_lat") && preferences.contains("marker_lng")) {
+            Log.i("MARKERRRRR", "#$!@$!@%#!%JHKL");
+            float lat = preferences.getFloat("marker_lat", 0f);
+            float lng = preferences.getFloat("marker_lng", 0f);
+            String destName = preferences.getString("address", "");
+            LatLng markerPosition = new LatLng(lat, lng);
+
+            Log.i("MARKERRRRRRRRRR", String.valueOf(markerPosition));
+
+            lastMarker = mMap.addMarker(new MarkerOptions().position(markerPosition).title(destName));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 15));
+
+        }
+
+    }
+
 
     /**
      * Register the active listener for user's location
@@ -393,6 +554,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onPause();
     }
 
+    /**
+     * Handles location changes and updates the UI accordingly.
+     */
     private class ActiveListener implements LocationListener {
 
         @Override
@@ -400,7 +564,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             onLocation(location);
             LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
             if (mMap != null) {
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18));
             }
         }
 
@@ -419,9 +583,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             registerListeners();
         }
     };
+
+    /**
+     * Handles the user action to set the destination location based on the selected building address
+     * Converts the address to GPS coordinates and updates the map display
+     * @param view The view that triggered this method
+     */
     public void setToLocation(View view) {
-        TextView EditLocation = findViewById(R.id.classroomInput);
-        String address = EditLocation.getText().toString().trim();
+        String address = dest;
         Geocoder geocoder = new Geocoder(this);
         try {
             List<Address> addresses = geocoder.getFromLocationName(address, 1);
@@ -446,8 +615,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 // Set pin on the map
                 LatLng destination = new LatLng(toLatitude, toLongitude);
                 lastMarker = mMap.addMarker(new MarkerOptions().position(destination).title(address));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 15));
+                //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 15));
                 LatLng currLocation = new LatLng(latitude, longitude);
+                saveInformation();      //< Save the information about destination location, name, and poly points
                 requestWalkingDirections(currLocation, destination);
             } else {
                 Toast.makeText(this, "Unable to find location. Please enter a valid address.", Toast.LENGTH_SHORT).show();
@@ -456,5 +626,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
             Toast.makeText(this, "Error finding location. Please try again.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    /**
+     * Save information to SharedPreferences
+     */
+    private void saveInformation() {
+        SharedPreferences.Editor editor = settings.edit();
+        if (lastPolyline != null) {
+            List<LatLng> polylinePoints = lastPolyline.getPoints();
+            String polylineJson = convertPolylinePointsToJson(polylinePoints);
+            editor.putString("polyline", polylineJson);
+        }
+        if (lastMarker != null) {
+            LatLng markerPosition = lastMarker.getPosition();
+            editor.putFloat("marker_lat", (float) markerPosition.latitude);
+            editor.putFloat("marker_lng", (float) markerPosition.longitude);
+        }
+        editor.putString("address", dest);
+        editor.apply();
+    }
+
+    /**
+     * Convert the poly points for directions to a json to store
+     * @param points Poly points to convert to a JSON
+     * @return jsonArray String of the poly points
+     */
+    private String convertPolylinePointsToJson(List<LatLng> points) {
+        JSONArray jsonArray = new JSONArray();
+        for (LatLng point : points) {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("lat", point.latitude);
+                jsonObject.put("lng", point.longitude);
+                jsonArray.put(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonArray.toString();
+    }
+
+    /**
+     * Convert the JSON array back to poly points
+     * @param json Poly points in JSON format
+     * @return Poly points converted from JSON
+     */
+    private List<LatLng> convertJsonToPolylinePoints(String json) {
+        List<LatLng> points = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                double lat = jsonObject.getDouble("lat");
+                double lng = jsonObject.getDouble("lng");
+                points.add(new LatLng(lat, lng));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return points;
     }
 }
